c3dba75968360716043cf401b4727ef63971bc2a
vladsundukov1-bit <vladsundukov1@gmail.com>
2026-04-27 10:51:27 +0300

    Сюндюков Владислав 26-ист-3

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,47 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+  private:
+    struct Node {
+        T data;
+        Node* next;
+        Node(const T& value) : data(value), next(nullptr) {}
+    };
+
+    Node* head;
+
+public:
+    TPQueue() : head(nullptr) {}
+    ~TPQueue() {
+        while (head != nullptr) {
+            Node* tmp = head;
+            head = head->next;
+            delete tmp;
+        }
+    }
+
+void push(const T& item) {
+        Node* nNode = new Node(item);
+if (item.prior > head->data.prior) {
+            nNode->next = head;
+            head = nNode;
+        } else {
+            Node* current = head;
+            while (current->next != nullptr && current->next->data.prior >= item.prior) {
+                current = current->next;
+            }
+            nNode->next = current->next;
+            current->next = nNode;
+        }
+    }
+
+T pop() {
+      Node* temp = head;
+      T result = head->data;
+      head = head->next;
+      delete temp;
+      return result;
+    }
 };
 
 struct SYM {
